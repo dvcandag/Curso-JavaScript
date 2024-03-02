2684710d263170b05931833ab03ffbc6c54b0b3b
dvcandag <dvcandag@gmail.com>
2024-03-02 17:40:38 -0500

    Agregando ejercicio 05

---
+++ Ejercicio_05.js
@@ -0,0 +1,9 @@
+/*Para completar el ejercicio 05 ¿Cómo imprimirías los números del 1 al 100 
+en la terminal utilizando un bucle while en JavaScript?*/
+
+
+let x = 1;
+while (x <= 100) {
+    console.log(x);
+    x++;
+}
