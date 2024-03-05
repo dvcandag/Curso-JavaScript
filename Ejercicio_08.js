e59b8c49602517520aa9663eafe26ceecd74083e
dvcandag <dvcandag@gmail.com>
2024-03-04 22:32:44 -0500

    Agregando ejercicio 08 - Funciones

--- Ejercicio_08.js
+++ Ejercicio_08.js
@@ -1,1 +1,10 @@
+/*Ejercicio 08: Aplicanco tu conocimiento de funciones mostrar mensaje de aviso tres veces*/
 
+function mostrarMensaje() {
+    document.write("Cuidado<br>");
+    document.write("Ingrese su documento correctamente<br>");
+}
+
+mostrarMensaje();
+mostrarMensaje();
+mostrarMensaje();
