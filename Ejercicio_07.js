42134afdf8b7f265e91d553f5ad0c7e17f18c7b3
dvcandag <dvcandag@gmail.com>
2024-03-04 22:22:47 -0500

    Añadiendo ejercicio 07

--- Ejercicio_07.js
+++ Ejercicio_07.js
@@ -1,1 +1,15 @@
+/*Ejercicio 07: Imaginemos que estamos organizando un evento especial, como una fiesta o una reunión. 
+Para asegurarnos de que nuestros invitados tengan una experiencia inolvidable, necesitamos 
+planificar cuidadosamente los gastos relacionados con la comida y la bebida. En este contexto, 
+queremos crear un programa en JavaScript que nos ayude a calcular los costos totales. A través 
+de simples operaciones aritméticas, variables y realizar cálculos en JavaScript. Ahora los desafío a que 
+realicen su propio desarrollo para calcular los costos totales de comida, bebida y la cuenta total.*/
 
+
+var totalComida = 200 + 10;
+var totalBebida = 2050 + 390;
+var total = totalComida + totalBebida;
+
+console.log("El total de comida es: " + totalComida);
+console.log("El total de bebida es: " + totalBebida);
+console.log("El total de la cuenta es: " + total);
