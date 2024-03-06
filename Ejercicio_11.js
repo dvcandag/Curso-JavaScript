e39f0e5e3b512c7b1f1fd0d397ea373999ac98cc
dvcandag <dvcandag@gmail.com>
2024-03-05 21:26:35 -0500

    Añadiendo ejercicio 11

--- Ejercicio_11.js
+++ Ejercicio_11.js
@@ -1,3 +1,15 @@
+/* Ejercicio 11: Solicitar al usuario que ingrese una cadena utilizando la función prompt() 
+y almacena el valor en una variable llamada cadena. Muestrar la cadena ingresada 
+al usuario utilizando document.write(). Calcular y muestrar la cantidad de caracteres 
+en la cadena utilizando cadena.length. Muestrar el primer carácter de la cadena 
+utilizando cadena.charAt(0). Muestrar los primeros tres caracteres de la cadena 
+utilizando cadena.substring(0, 3). Verifica si la subcadena 'hola' está presente 
+en la cadena utilizando cadena.indexOf('hola'). Si está presente, 
+muestra 'Se ingresó la subcadena hola'; de lo contrario, 
+muestra 'No se ingresó la subcadena hola'. Convierte la cadena a mayúsculas 
+utilizando cadena.toUpperCase() y muestrar el resultado. Conviertir la cadena 
+a minúsculas utilizando cadena.toLowerCase() y muestrar el resultado */
+
 let cadena = prompt('Ingrese una cadena:');
         document.write('La cadena ingresada es:' + cadena);
         document.write('<br>');
