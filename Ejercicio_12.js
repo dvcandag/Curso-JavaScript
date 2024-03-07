f92bdc8bdf8a44d651f22f47dad78d94dd1a1857
dvcandag <dvcandag@gmail.com>
2024-03-06 21:23:03 -0500

    Agregando ejercicio 12

--- Ejercicio_12.js
+++ Ejercicio_12.js
@@ -1,1 +1,24 @@
+function Cliente(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+    this.depositar = depositar;
+    this.extraer = extraer;
+}
+
+function depositar(dinero) {
+    this.saldo = this.saldo + dinero;
+}
+
+function extraer(dinero) {
+    this.saldo = this.saldo - dinero;
+}
+
+let cliente1;
+cliente1 = new Cliente('diego', 1200);
+document.write('Nombre del cliente:' + cliente1.nombre + '<br>');
+document.write('Saldo actual:' + cliente1.saldo + '<br>');
+cliente1.depositar(120);
+document.write('Saldo luego de depositar S/ 2220---->' + cliente1.saldo + '<br>');
+cliente1.extraer(1000);
+document.write('Saldo luego de extraer S/ 120---->' + cliente1.saldo + '<br>');
 
