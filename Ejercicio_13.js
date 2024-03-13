fcdba9bebeeb9c2eebdbe3f58472788334b8e5e1
dvcandag <dvcandag@gmail.com>
2024-03-12 20:55:31 -0500

    Añadiendo ejercicio 13 - Hipervinculos

--- Ejercicio_13.js
+++ Ejercicio_13.js
@@ -1,1 +1,11 @@
+/* 
+Ejercicio 13: 
+
+Desarrolla una clase "Hipervinculo" en js que represente un hipervínculo HTML 
+con dos atributos: "direccion" y "titulo". La clase debe incluir un método llamado 
+"retornar_hipervinculo" que devuelve el hipervínculo en formato HTML válido. Además, 
+crea una lista llamada "vector" que contendrá tres objetos de la clase Hipervinculo. 
+Asegúrate de seguir el estilo de codificación js, proporcionar comentarios 
+explicativos. Tu solución debe entregarse en un archivo .js */
+
 
