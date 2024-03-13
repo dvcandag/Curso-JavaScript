8b2f8ca5bd8827a3e6a5119f6e2e4de9d07d976b
dvcandag <dvcandag@gmail.com>
2024-03-12 21:13:53 -0500

    Añadiendo ejercicio 14 - Atributos y Métodos

--- Ejercicio_14.js
+++ Ejercicio_14.js
@@ -1,1 +1,19 @@
+/*
+Ejercicio 56:
+Desarrollar una clase llamada "Cliente", diseñada para representar a un cliente de un banco
+y permitir operaciones básicas de gestión de saldo. Los saldos deben mostrarse en moneda peruana. 
+La clase debe tener los siguientes atributos y métodos:
 
+Atributos:
+a) nombre: Almacena el nombre del cliente.
+b) deposito: Guarda el saldo actual del cliente, inicialmente en 0.
+
+Métodos:
+a) constructor(nombre): Un constructor que inicializa el nombre del cliente y su saldo.
+b) imprimir(): Un método que imprime el nombre y el saldo del cliente.
+c) depositar(monto): Permite al cliente depositar una cantidad específica.
+d) extraer(monto): Permite al cliente extraer una cantidad específica.
+
+Proporcionar comentarios explicativos, tu solución debe entregarse en un archivo .js 
+que se ejecute en un navegador web usando un index.html
+*/
