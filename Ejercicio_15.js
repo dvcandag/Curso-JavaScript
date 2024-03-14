9f3c5a9b3c0b7aa475c35af21ad2c53292207ff3
dvcandag <dvcandag@gmail.com>
2024-03-13 20:15:07 -0500

    Entregando ejercicio 15 - Listas

--- Ejercicio_15.js
+++ Ejercicio_15.js
@@ -12,3 +12,24 @@ individuales, multiplicación de elementos y inclusión de elementos de diferent
 
 Proporcionar comentarios explicativos. Tu solución debe entregarse en un archivo .js que se ejecute en el terminal 
 */
+
+//SOLUCION
+// Creación de listas en JavaScript
+let vector1 = [undefined];  // Creación de una lista con un elemento undefined
+vector1[0] = 1;  // Modificación del primer elemento de la lista
+
+let vector2 = [undefined];  // Creación de otra lista con un elemento undefined
+vector2[0] = 6;  // Modificación del primer elemento de la segunda lista
+
+let vector3 = new Array(5);  // Creación de una lista con cinco elementos undefined
+vector3[0] = 4;  // Modificación del primer elemento de la tercera lista
+
+let vector4 = [3, 70, 'Vitto Andagua'];  // Creación de una lista con elementos 3, 70 y 'Vitto Andagua'
+let vector5 = [1, 70, 'Juan Lozada'];  // Creación de otra lista con los mismos elementos que la anterior
+
+// Imprimir elementos de las listas
+console.log(vector1[0]);  // Imprimir el primer elemento de la lista vector1
+console.log(vector2[0]);  // Imprimir el primer elemento de la lista vector2
+console.log(vector3[0]);  // Imprimir el primer elemento de la lista vector3
+console.log(vector4[0]);  // Imprimir el primer elemento de la lista vector4
+console.log(vector5[0]);  // Imprimir el primer elemento de la lista vector5
