8d8562470af0f40685eb1ef3f93557447a56a5bf
dvcandag <dvcandag@gmail.com>
2024-03-13 21:23:08 -0500

    Entregando ejercicio 16

--- Ejercicio_16.js
+++ Ejercicio_16.js
@@ -14,3 +14,31 @@ Instrucciones:
 4.- Además, indica qué cliente ha realizado más depósitos.
 
 */
+//SOLUCION
+
+let depositos = {};
+
+while (true) {
+    let nro = parseInt(prompt('Ingrese nro de cliente (0 para finalizar): '));
+    if (nro === 0) {
+        break;
+    }
+    let monto = parseInt(prompt('Ingrese monto a depositar: '));
+
+    if (nro in depositos) {
+        depositos[nro]['cantidad'] += 1; // Incrementar el contador de depósitos para el cliente
+        depositos[nro]['monto_total'] += monto; // Sumar el monto depositado
+    } else {
+        depositos[nro] = {'cantidad': 1, 'monto_total': monto}; // Iniciar el contador de depósitos y el monto total para el cliente
+    }
+}
+
+let cantidad_depositos = Object.values(depositos).reduce((acc, cliente) => acc + cliente.cantidad, 0);
+let suma_total = Object.values(depositos).reduce((acc, cliente) => acc + cliente.monto_total, 0);
+
+document.write('Cantidad de depósitos: ' + cantidad_depositos + '<br>');
+document.write('Total depositado por todos los clientes: ' + suma_total + '<br>');
+
+// Encontrar al cliente que ha depositado más veces
+let cliente_mas_depositos = Object.keys(depositos).reduce((a, b) => depositos[a].cantidad > depositos[b].cantidad ? a : b);
+document.write('Cliente que ha depositado más veces: ' + cliente_mas_depositos + ' con ' + depositos[cliente_mas_depositos].cantidad + ' depósitos.');
