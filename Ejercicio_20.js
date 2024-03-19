6cf7fac85a92bdcb99042a017b78387f9c1d9037
dvcandag <dvcandag@gmail.com>
2024-03-18 21:45:27 -0500

    Añadiendo ejercicio 20 - Condicionales

---
+++ Ejercicio_20.js
@@ -0,0 +1,13 @@
+/*
+Ejercicio 19: Generar contraseña
+Vitto tiene N dólares. Luis tiene la mitad de la cantidad de dinero que Vitto posee, mientras que 
+José tiene la mitad de la suma de lo que Vitto y Luis tienen juntos. Se desea calcular el promedio
+de la cantidad de dinero que poseen entre los tres y redondearlo al número entero más cercano.
+
+
+Proporcionar comentarios explicativos. Tu solución debe entregarse en un archivo .js y html
+ejecutandose satisfactoriamente en el terminal de su entorno de desarrollo.
+*/
+
+
+
