eacef1fa974a338633525daa52788a95ca1cb3fa
dvcandag <dvcandag@gmail.com>
2024-03-18 21:49:30 -0500

    Entregando ejercicio 20 - Condicionales

--- Ejercicio_20.js
+++ Ejercicio_20.js
@@ -10,4 +10,22 @@ ejecutandose satisfactoriamente en el terminal de su entorno de desarrollo.
 */
 
 
+//SOLUSION
+
+// Definición de la cantidad de dinero que tiene Vitto
+let vitto = 47; // Cantidad de dinero que tiene Vitto (cambia este valor según sea necesario)
+
+// Calculando la cantidad de dinero que tiene Luis, que es la mitad de lo que tiene Vitto
+let luis = vitto / 2; // Luis tiene la mitad de lo que tiene Vitto
+
+// Calculando la cantidad de dinero que tiene José, que es la mitad de la suma de lo que tienen Vitto y Luis juntos
+let jose = (vitto + luis) / 2; // José tiene la mitad de la suma de lo que tienen Vitto y Luis juntos
+
+// Calculando el promedio de la cantidad de dinero que poseen entre Vitto, Luis y José
+let promedio = Math.round((vitto + luis + jose) / 3); // Calculando el promedio y redondeándolo al número entero más cercano
+
+// Mostrando el promedio en la consola
+console.log(`El promedio de la cantidad de dinero que poseen entre Vitto, Luis y José es de: ${promedio}`);
+
+
 
