b948c51df81d4b6b0a9f2cfad7e924bdcfac5bfc
dvcandag <dvcandag@gmail.com>
2024-03-21 20:57:11 -0500

    Entregando ejercicio 21 - Encontrar números pares en un arreglo

--- Ejercicio_21.js
+++ Ejercicio_21.js
@@ -10,3 +10,26 @@ en la terminal del entorno de desarrollo.
 
 */
 
+//SOLUSION
+
+// Definición de la función "pares" que encuentra números pares en un arreglo 
+function pares(arr) {
+    // Crear una nueva matriz para almacenar los números pares
+    let paresArray = [];
+
+    // Iterar sobre cada elemento del arreglo proporcionado
+    for (let num of arr) {
+        // Verificar si el número es par
+        if (num % 2 === 0) {
+            // Si es par, agregarlo a la matriz de números pares
+            paresArray.push(num);
+        }
+    }
+
+    // Devolver la matriz que contiene los números pares
+    return paresArray;
+}
+
+// Ejemplos de uso de la función 'pares'
+console.log(pares([1, 2, 3, 4, 5, 6])); // se imprime: [2, 4, 6]
+console.log(pares([10, 11, 12, 13, 14])); // se imprime: [10, 12, 14]
