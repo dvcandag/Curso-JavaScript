f522bb6ff585047b8b0c176e8898102cf8dc1e04
dvcandag <dvcandag@gmail.com>
2024-03-21 21:38:26 -0500

    Añadiendo ejercicio 23 - Traducir palabras a números

--- Ejercicio_23.js
+++ Ejercicio_23.js
@@ -1,1 +1,13 @@
+/*
+Ejercicio 23: Traducir palabras a números
+
+Crear una función denominada 'Palabras_a_Numeros' que acepte un arreglo de cadenas y devuelva un nuevo arreglo 
+traduciendo cada palabra a su equivalente numérico del 0 al 9. En caso de que una palabra no sea un número, se traducirá como -1.
+
+Proporcionar un ejemplo agregar comentarios explicativos de su codificación. 
+Tu solución debe entregarse en un archivo .js ejecutandose satisfactoriamente 
+en el terminal de su entorno de desarrollo.
+
+*/
+
 
