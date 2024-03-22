5a4f756bb51852ec9fdb6fd09c6023d2a9a2fe09
dvcandag <dvcandag@gmail.com>
2024-03-21 20:51:45 -0500

    Añadiendo ejercicio 21 - Encontrar números pares en un arreglo

--- Ejercicio_21.js
+++ Ejercicio_21.js
@@ -1,1 +1,12 @@
+/*
+Ejercicio 21: Encontrar números pares en un arreglo
+
+Desarrollar una función denominada 'pares' que acepte una matriz de números y 
+devuelva una nueva matriz que contenga únicamente los números pares.
+
+Incluir dos ejemplos del caso y agregar comentarios descriptivos. 
+La solución debe ser presentada en un archivo .js y ejecutarse exitosamente
+en la terminal del entorno de desarrollo.
+
+*/
 
