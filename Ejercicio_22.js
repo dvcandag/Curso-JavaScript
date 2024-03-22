4ab9f14fc6492e8205ae0a99df27de5e4530bb8a
dvcandag <dvcandag@gmail.com>
2024-03-21 21:17:17 -0500

    Añadiendo ejercicio 22 - Traducir números a palabras

--- Ejercicio_22.js
+++ Ejercicio_22.js
@@ -1,1 +1,12 @@
+/*
+Ejercicio 22: Traducir números a palabras
+
+Crear una función denominada "Numeros_a_Palabras" que acepte un arreglo de números, 
+donde cada número se encuentra en el rango de 0 a 9, y devuelva un nuevo arreglo que contenga 
+la versión en palabras de cada número."
+
+Proporcionar un ejemplo agregar comentarios explicativos de su codificación. 
+Tu solución debe entregarse en un archivo .js ejecutandose satisfactoriamente 
+en el terminal de su entorno de desarrollo.
+*/
 
