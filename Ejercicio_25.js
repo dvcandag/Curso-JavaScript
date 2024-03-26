715af34c90a4cd8b0b1b144e4e1a0b5f9cdb7d29
dvcandag <dvcandag@gmail.com>
2024-03-25 20:30:51 -0500

    Añadiendo ejercicio 25  Imprimir el número mayor

---
+++ Ejercicio_25.js
@@ -0,0 +1,10 @@
+/*
+Ejercicio 25: Imprimir el número mayor
+
+Desarrolla un programa que solicite al usuario ingresar tres números y luego muestre en la pantalla el número más grande de los tres.
+
+Incluir comentarios explicativos en tu código proporcionando con claridad sobre cómo funciona.
+Tu solución debe ser entregada en un archivo .js que se ejecute correctamente en el navegador de tu preferencia.
+
+*/
+
