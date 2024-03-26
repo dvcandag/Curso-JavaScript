52f8153fe989747c0ddde3fa1c400cc8f4841bbb
dvcandag <dvcandag@gmail.com>
2024-03-25 20:40:51 -0500

    Añadiendo ejercicio 26 - Saludo Personalizado

---
+++ Ejercicio_26.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 26: Saludo Personalizado
+
+Crea un programa que, en solo dos líneas, solicite al usuario que ingrese su nombre mediante un (prompt)
+y luego muestre un mensaje que diga "Hola nombreUsuario".
+
+Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+La solución debe entregarse en un archivo .js que se ejecute correctamente en el navegador de tu elección.
+*/
