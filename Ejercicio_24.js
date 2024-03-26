9ff762ac6696da162b743790d4a1127f27e3526d
dvcandag <dvcandag@gmail.com>
2024-03-25 20:19:02 -0500

    Agregando ejercicio 24 - Comprobar si son divisible

--- Ejercicio_24.js
+++ Ejercicio_24.js
@@ -1,1 +1,9 @@
+/*
+Ejercicio 24: Comprobar si son divisible
 
+Crea un programa que solicite un número y determine si es divisible entre 2, 3, 5 o 7 (solamente se necesita 
+verificar si es divisible por uno de estos cuatro).
+
+Incluye comentarios explicativos de tu código para comprobar claridad sobre su funcionamiento.
+Tu solución debe ser entregada en un archivo .js que se ejecute satisfactoriamente en el navegador de su preferencia
+*/
