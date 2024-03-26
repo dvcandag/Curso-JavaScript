cc8ca36a389211d506bfb597ef7f62fb42ac1996
dvcandag <dvcandag@gmail.com>
2024-03-25 20:50:24 -0500

    Entregando ejercicio 26 -  Saludo Personalizado

--- Ejercicio_26.js
+++ Ejercicio_26.js
@@ -7,3 +7,11 @@ y luego muestre un mensaje que diga "Hola nombreUsuario".
 Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
 La solución debe entregarse en un archivo .js que se ejecute correctamente en el navegador de tu elección.
 */
+
+//SOLUCION
+
+// Solicitar al usuario que ingrese su nombre mediante un prompt y almacenarlo en la variable nombreUsuario
+var nombreUsuario = prompt("Por favor, ingresa tu nombre:");
+
+// Mostrar un mensaje de saludo personalizado utilizando el nombre ingresado por el usuario
+alert("Hola " + nombreUsuario);
