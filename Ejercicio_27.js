8d69c30eb6506ff9fe751f67a3022d1e1dee32c0
dvcandag <dvcandag@gmail.com>
2024-03-28 21:58:26 -0500

    Añadiendo ejercicio 27 - Calculadora

--- Ejercicio_27.js
+++ Ejercicio_27.js
@@ -1,1 +1,16 @@
+/*
+Ejercicio 27: Calculadora
+Desarrollar una interfaz gráfica de usuario (GUI) denominada "Calculadora" en JavaScript, que funcione como una calculadora.
+
+Esta interfaz debe incluir los siguientes elementos:
+
+1.- Cuatro etiquetas: "Valor 1", "Valor 2", "Resultado" y "Operaciones".
+2.- Cuatro botones de radio: "Sumar", "Restar", "Multiplicar" y "Dividir".
+3.- Tres campos de entrada de texto: los dos primeros para ingresar valores ("Valor 1" y "Valor 2") y el tercero para mostrar 
+    el resultado ("Resultado"). El campo de entrada de texto para el resultado debe ser de solo lectura.
+4.- Un botón "Calcular", que al ser presionado ejecutará la operación matemática correspondiente según la selección del botón de radio.
+
+Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+La solución debe entregarse en un archivo .js que se ejecute correctamente en el navegador de tu elección.
+*/
 
