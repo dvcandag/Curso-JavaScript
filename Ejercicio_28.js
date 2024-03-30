682e92f927d3054ce4c646b0a2d8387ea39bf0f5
dvcandag <dvcandag@gmail.com>
2024-03-29 20:59:43 -0500

    Añadiendo ejercicio 28 - Suma de números

--- Ejercicio_28.js
+++ Ejercicio_28.js
@@ -1,1 +1,8 @@
+/*
+Ejercicio 27: Suma de números
+Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números
+
+Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+La solución debe entregarse en un archivo .js que se ejecute correctamente en el navegador de tu elección.
+*/
 
