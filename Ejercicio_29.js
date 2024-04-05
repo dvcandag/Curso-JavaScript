8d28587e02a0496eadac8066089d41f41f8b8d68
dvcandag <dvcandag@gmail.com>
2024-04-04 22:00:36 -0500

    Añadiendo ejercicio 29: Conteo de vocales dentro de una frase

--- Ejercicio_29.js
+++ Ejercicio_29.js
@@ -1,1 +1,13 @@
 
+/*
+Ejercicio 29: Conteo de vocales dentro de una frase
+
+Crea un programa que solicite al usuario ingresar una frase y luego muestre las vocales presentes en esa frase.
+
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+2.- La solución debe entregarse en un archivo .js que se ejecute correctamente en el navegador de tu elección.
+3.- Utilizar la función `alert()` para mostrar un mensaje emergente en la pantalla.
+
+*/
+
+
