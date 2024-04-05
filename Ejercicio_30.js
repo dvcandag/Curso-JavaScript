11767ff45c70aebf733c4bf6eec4d81ea3b3dc0b
dvcandag <dvcandag@gmail.com>
2024-04-04 22:19:05 -0500

    Añadiendo ejercicio 29: divisores de un número

--- Ejercicio_30.js
+++ Ejercicio_30.js
@@ -1,1 +1,12 @@
+/*
+Ejercicio 29: divisores de un número
+
+Crear un programa que muestre en la pantalla los divisores de un número específico.
+
+
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+2.- La solución debe entregarse en un archivo .js que se ejecute correctamente en el navegador de tu elección.
+3.- Utilizar las etequietas html para mostrar resultados en la pantalla.
+
+*/
 
