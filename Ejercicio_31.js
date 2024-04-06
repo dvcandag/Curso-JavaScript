b4c6f62e9dd5f3f4a379e66c9486a2d97a6933da
dvcandag <dvcandag@gmail.com>
2024-04-05 21:29:16 -0500

    Añadiendo ejercicio 31: kilómetros recorridos y la cantidad de litros consumidos

--- Ejercicio_31.js
+++ Ejercicio_31.js
@@ -1,1 +1,18 @@
+/*
+Ejercicio 31: kilómetros recorridos y la cantidad de litros consumidos
+
+Crea un script que pida al usuario que introduzca la cantidad de kilómetros recorridos por su 
+automóvil y la cantidad de litros consumidos. Luego, el programa debe calcular y mostrar el 
+consumo de combustible por kilómetro.
+
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+2.- La solución debe entregarse en un archivo .js que se ejecute correctamente en el navegador de tu elección.
+3.- Utilizar las etequietas html para mostrar resultados en la pantalla.
+
+*/
+
+
+
+
+
 
