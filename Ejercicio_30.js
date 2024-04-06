2c19306507cc1dada6273d56f31801e23347d3d0
dvcandag <dvcandag@gmail.com>
2024-04-05 21:12:31 -0500

    Entregando ejercicio 29: divisores de un número

--- Ejercicio_30.js
+++ Ejercicio_30.js
@@ -10,3 +10,25 @@ Crear un programa que muestre en la pantalla los divisores de un número especí
 
 */
 
+// SOLUCIÓN
+
+// Solicitamos al usuario que ingrese un número mediante la función prompt()
+let numero = parseInt(prompt("Por favor, ingresa un número:"));
+
+// Creamos una variable para almacenar los divisores del número
+let divisores = [];
+
+// Recorremos todos los números desde 1 hasta el número ingresado por el usuario
+for (let i = 1; i <= numero; i++) {
+    // Verificamos si el número es divisor del número ingresado por el usuario
+    if (numero % i === 0) {
+        // Si es divisor, lo añadimos a la lista de divisores
+        divisores.push(i);
+    }
+}
+
+// Creamos un mensaje con los divisores encontrados
+let mensaje = "Los divisores de " + numero + " son: " + divisores.join(", ");
+
+// Mostramos el mensaje en la pantalla utilizando las etiquetas HTML
+document.write("<h2>" + mensaje + "</h2>");
