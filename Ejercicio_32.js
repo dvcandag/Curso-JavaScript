410138661fd7df594e41a3a031dd3b0012d834ba
dvcandag <dvcandag@gmail.com>
2024-04-08 19:32:04 -0500

    Añadiendo ejercicio 32: diferencia entre los variales var y let

--- Ejercicio_32.js
+++ Ejercicio_32.js
@@ -1,1 +1,13 @@
+/*
+Ejercicio 32: diferencia entre los variales var y let
+
+Supongamos que estás desarrollando una aplicación de lista de tareas. Se te pide que implementes 
+una función que agregue nuevas tareas a la lista. Sin embargo, hay un problema: algunas tareas 
+son urgentes y otras no lo son tanto. Además, debes tener cuidado con el alcance de las variables que uses. 
+
+1.- Debe utilicar el var y let para desarrollar y hacer comparativo (Aplicar temasrios de variales var y let)
+2.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+3.- La solución debe entregarse en un archivo .js que se ejecute correctamente en el navegador de tu elección.
+4.- Utilizar las etequietas html para mostrar resultados en la pantalla.
+*/
 
