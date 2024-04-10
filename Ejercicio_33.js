e91f84822fd19b0b983237d267a74633b5f51fb6
dvcandag <dvcandag@gmail.com>
2024-04-09 20:34:41 -0500

    Añadiendo ejercicio 33: Tipos de datos - Constantes (const)

--- Ejercicio_33.js
+++ Ejercicio_33.js
@@ -1,1 +1,19 @@
+/*
+Ejercicio 33: Tipos de datos - Constantes (const)
+
+Desarrollar un programa en JavaScript y HTML que pueda determinar tanto el área como el perímetro 
+de un círculo. Es importante que el usuario pueda ingresar el radio del círculo para que 
+el programa realice los cálculos adecuadamente.
+
+Para asegurarnos de que nuestros cálculos sean precisos y consistentes, quiero que utilicen 
+constantes para almacenar el valor de π (pi). Recuerden que π es una constante matemática que 
+no cambia y no debe ser modificada durante la ejecución del programa.
+
+Instrucciones:
+
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su codificación.
+2.- La solución debe ejecutarse correctamente en el navegador de tu elección.
+3.- Para su calificación, deberá explicar su codificación y hacer pruebas de depuración 
+
+*/
 
