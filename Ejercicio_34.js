93ae81b961fad6b61f70f2860395c6d7bf92ccde
dvcandag <dvcandag@gmail.com>
2024-04-09 21:00:59 -0500

    Añadiendo ejercicio 34: Tipos de datos - Cadenas de Texto (Strings)

--- Ejercicio_34.js
+++ Ejercicio_34.js
@@ -1,1 +1,26 @@
+/*
+Ejercicio 34: Tipos de datos - Cadenas de Texto (Strings)
+
+Escribe un programa en JavaScript, con entrada desde HTML, que solicite al usuario su nombre completo y su país de origen. Luego, muestra 
+un mensaje de bienvenida personalizado que incluya su nombre y su país.
+
+Instrucciones:
+
+1.- Se debe solicitar al usuario que ingrese su nombre completo, asegurándose de que la primera letra de cada palabra (nombre y apellido) 
+    esté en mayúscula.
+2.- El país ingresado automáticamente debe convertirse a mayúsculas para garantizar consistencia.
+3.- Como salida, el nombre completo del usuario debe mostrarse en mayúsculas y el nombre del país en minúsculas con la primera letra en mayúscula.
+4.- Asegúrate de incluir comentarios explicativos en tu código para facilitar la comprensión de su funcionamiento.
+5.- Verificar que la solución se ejecute correctamente en cualquier navegador.
+6.- Para evaluar tu solución, deberás explicar tu código y realizar pruebas de depuración.
+7.- El resultado esperado es:
+    
+    Entrada:
+        Nombre: Juan Pérez
+        País: PERÚ
+
+    Salida:
+        ¡Bienvenido, JUAN PÉREZ! Nos alegra tenerte desde Perú.
+
+*/
 
