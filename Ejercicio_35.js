8d55adc3c721ed2275d9dad9c84d8f9a92c16ded
dvcandag <dvcandag@gmail.com>
2024-04-11 21:23:38 -0500

    Añadiendo ejercicio 35: Tipos de datos - Cadenas de Texto (Strings)

--- Ejercicio_35.js
+++ Ejercicio_35.js
@@ -1,1 +1,15 @@
+/*
+Ejercicio 35: Tipos de datos - Cadenas de Texto (Strings)
 
+Desarrolla un programa en JavaScript que solicite al usuario su nombre, edad y ciudad de residencia. Luego, utiliza
+las template strings para mostrar un mensaje personalizado que incluya esta información.
+
+
+Requisitos:
+
+1.- Utilizar template strings para construir el mensaje personalizado.
+2.- Asegúrate de que la primera letra del nombre esté en mayúscula y que la ciudad se muestre con la primera letra en mayúscula.
+3.- Incluye comentarios explicativos en tu código para facilitar su comprensión.
+4.- Verifica que la solución se ejecute correctamente en cualquier navegador.
+5.- Para evaluar tu solución, deberás explicar tu código y realizar pruebas de depuración.
+*/
