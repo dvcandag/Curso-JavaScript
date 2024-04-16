6309a0820363c79fed0c227383d36fc69bbfa911
dvcandag <dvcandag@gmail.com>
2024-04-15 19:56:52 -0500

    Añadiendo Ejercicio 36: Tipos de datos - Números (Numbers)

--- Ejercicio_36.js
+++ Ejercicio_36.js
@@ -1,1 +1,16 @@
+/*
+Ejercicio 36: Tipos de datos - Números (Numbers)
+
+Desarrollar un programa en JavaScript y HTML que calcule el área de un triángulo. El usuario debe poder 
+ingresar el valor de la base y la altura del triángulo.
+
+Requisitos:
+1.- Solicitar al usuario que ingrese las longitudes de los tres lados del triángulo.
+2.- Calcular los ángulos del triángulo utilizando la ley de los cosenos.
+3.- Calcular el área del triángulo utilizando la fórmula de Herón.
+4.- Mostrar el dibujo del triángulo en HTML, incluyendo las longitudes de los lados y los ángulos calculados.
+
+*/
+
+
 
