8eed19aeb543d4fa3c154719717cc165ca0da710
dvcandag <dvcandag@gmail.com>
2024-04-16 20:47:47 -0500

    Añadiendo el  ejercicio 37: Tipos de datos - Booleans

--- Ejercicio_37.js
+++ Ejercicio_37.js
@@ -1,1 +1,16 @@
+/*
+Ejercicio 37: Tipos de datos - Booleans
+
+Jhon está desarrollando un programa para ayudar a los estudiantes a determinar si han aprobado o no un curso basado en sus 
+calificaciones. El programa debe tomar las calificaciones de tres exámenes (cada una en un rango del 0 al 20) y determinar 
+si el estudiante ha aprobado o no el curso. El curso se considera aprobado si la calificación promedio es igual o mayor a 13.
+
+Requisitos:
+
+1.- El programa debe solicitar al usuario ingresar por console las calificaciones de los tres exámenes.
+2.- Calcula el promedio de las tres calificaciones.
+3.- Utiliza un booleano para determinar si el estudiante ha aprobado el curso o no.
+4.- Muestra un mensaje que indique si el estudiante ha aprobado el curso o no, basado en el valor del booleano.
+
+*/
 
