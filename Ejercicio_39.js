ef83783df7831db24c3507ca0fe55e3d5ce1e067
dvcandag <dvcandag@gmail.com>
2024-04-22 20:38:29 -0500

    Añadiendo ejercicio 39: Tipos de datos - Funciones

--- Ejercicio_39.js
+++ Ejercicio_39.js
@@ -1,1 +1,23 @@
+/*
+Ejercicio 39: Tipos de datos - Funciones
 
+Calculadora de Impuestos:
+
+Desarrolla una aplicación en JavaScript que permita calcular el impuesto sobre la renta de una persona según su salario anual.
+La aplicación debe solicitar al usuario que ingrese su salario anual y luego calcular y mostrar el impuesto correspondiente.
+
+Requisitos:
+
+1.- La aplicación debe utilizar una función para calcular el impuesto sobre la renta. Esta función tomará el salario anual 
+    como parámetro y devolverá el impuesto calculado.
+
+2.- El impuesto sobre la renta se calcula de acuerdo con la siguiente tabla:
+    A.- Para salarios menores o iguales a S/ 50, el impuesto es del 10% del salario.
+    B.- Para salarios entre S/ 50 y S/ 100 el impuesto es del 15% del salario.
+    C.- Para salarios mayores a S/ 100, el impuesto es del 20% del salario.
+
+3.- Después de calcular el impuesto, la aplicación debe mostrar un mensaje que indique el salario anual ingresado por el usuario
+    y el impuesto calculado.
+
+4.- La aplicación debe permitir al usuario realizar múltiples cálculos ingresando salarios anuales diferentes en cada ejecución.
+*/
