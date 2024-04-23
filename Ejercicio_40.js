4c8ebc568a92d486bd98dae00ee3d8f8a6ac8d64
dvcandag <dvcandag@gmail.com>
2024-04-22 20:50:14 -0500

    Añadiendo ejercicio 40: Tipos de datos - Arreglos (Arrays)

--- Ejercicio_40.js
+++ Ejercicio_40.js
@@ -1,1 +1,16 @@
+/*
+Ejercicio 40: Tipos de datos - Arreglos (Arrays)
 
+Calculadora de gastos Semanales
+
+Desarrolla una aplicación que permita al usuario calcular sus gastos semanales. La aplicación debe solicitar 
+al usuario que ingrese los gastos de cada día de la semana y luego mostrar el total de gastos al finalizar la semana.
+
+Requisitos:
+
+1.- Utiliza un arreglo para almacenar los gastos de cada día de la semana.
+2.- Implementa una función para calcular el total de gastos. Esta función debe tomar como parámetro el arreglo de 
+    gastos y sumar todos los valores.
+3.- Muestra al usuario un mensaje que solicite ingresar los gastos para cada día de la semana.
+4.- Después de que el usuario ingrese los gastos para cada día, calcula y muestra el total de gastos al finalizar la semana.
+*/
