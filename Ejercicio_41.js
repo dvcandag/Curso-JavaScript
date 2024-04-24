9a7c2f090147c66af7e1e52d402031043fae43fd
dvcandag <dvcandag@gmail.com>
2024-04-23 21:01:07 -0500

    Añadiendo ejercicio 41: Tipos de datos - Objetos

--- Ejercicio_41.js
+++ Ejercicio_41.js
@@ -1,1 +1,18 @@
 
+/*
+Ejercicio 41: Tipos de datos - Objetos
+
+Problema: Registro de Productos
+
+Desarrolla una aplicación en JavaScript para llevar un registro de los productos en un almacén. Cada producto tendrá los siguientes datos:
+    - Nombre
+    - Precio
+    - Cantidad en stock
+
+Requisitos:
+    1.- Utiliza un objeto para representar a cada producto, donde las claves del objeto sean nombre, precio y cantidad.
+    2.- Utiliza un arreglo para almacenar todos los productos registrados.
+    3.- La función agregarProducto debe tomar como parámetros los datos del nuevo producto y agregar un nuevo objeto producto al arreglo de productos.
+    4.- La función mostrarProductos debe recorrer el arreglo de productos y mostrar en la consola los datos de cada producto.
+
+*/
