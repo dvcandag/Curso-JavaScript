87d0bfd38b1e1f890e2c579fd7927e5b5782c704
dvcandag <dvcandag@gmail.com>
2024-04-25 20:40:06 -0500

    Añadiendo ejercicio 44: Estructuras de Control - Tipos de Operadores

--- Ejercicio_44.js
+++ Ejercicio_44.js
@@ -1,1 +1,13 @@
+/*
+Ejercicio 44: Estructuras de Control - Tipos de Operadores
 
+Escribe un programa que solicite al usuario que ingrese una contraseña. La contraseña debe cumplir con ciertos criterios, como 
+tener al menos 8 caracteres de longitud, contener al menos una letra mayúscula, una letra minúscula, dos números y dos caracteres especiales. 
+El programa debe validar si la contraseña ingresada cumple con todos los criterios y mostrar un mensaje apropiado.
+
+Requisitos:
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+2.- La solución debe entregarse en un archivo .js que se ejecute correctamente.
+3.- Utilizar las etequietas html para mostrar resultados en la pantalla.
+
+*/
