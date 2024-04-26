aa4e63e88784e9003baa404e1f8a2867f4c2616c
dvcandag <dvcandag@gmail.com>
2024-04-25 20:35:26 -0500

    Añadiendo ejercicio 43: Estructuras de Control - Tipos de Operadores

---
+++ Ejercicio_43.js
@@ -0,0 +1,12 @@
+/*
+Ejercicio 43: Estructuras de Control - Tipos de Operadores
+
+Crea un programa que solicite al usuario ingresar el precio original de un producto y el porcentaje de descuento aplicado. Luego, 
+calcula el precio final después del descuento y muestra el resultado.
+
+Requisitos:
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+2.- La solución debe entregarse en un archivo .js que se ejecute correctamente.
+3.- Utilizar las etequietas html para mostrar resultados en la pantalla.
+
+*/
