f332ac56dac509fdfb02b72c383234e020f9a37b
dvcandag <dvcandag@gmail.com>
2024-04-25 20:30:38 -0500

    Añadiendo ejercicio 42: Estructuras de Control - Tipos de Operadores

--- Ejercicio_42.js
+++ Ejercicio_42.js
@@ -1,1 +1,12 @@
+/*
+Ejercicio 42: Estructuras de Control - Tipos de Operadores
 
+Crea un programa que permita al usuario convertir temperaturas entre Celsius, Fahrenheit y Kelvin. Solicita al usuario 
+que ingrese un valor de temperatura junto con la escala de origen y la escala de destino, y luego realiza la conversión 
+utilizando los operadores adecuados.
+
+Requisitos:
+1.- Mostrar el resultado usando HTML.
+2-. Válidar las escalas de temperatura adecuadas.
+
+*/
