34449dd1737f23e5795723913ae5f3e023b9ae5c
dvcandag <dvcandag@gmail.com>
2024-04-26 20:08:25 -0500

    Añadiendo ejercicio 46: Estructuras de Control - Condicionales

--- Ejercicio_56.js
+++ Ejercicio_56.js
@@ -1,1 +1,14 @@
+/*
+Ejercicio 46: Estructuras de Control - Condicionales
 
+Crea un programa que simule el funcionamiento de un cajero automático. Solicita al usuario ingresar 
+el monto que desea retirar. Si el monto es menor que el saldo disponible en la cuenta, muestra un 
+mensaje indicando que la transacción fue exitosa y actualiza el saldo disponible. Si el monto es mayor 
+que el saldo disponible, muestra un mensaje indicando que no hay suficientes fondos en la cuenta.
+
+Requisitos:
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+2.- La solución debe entregarse en un archivo .js que se ejecute correctamente.
+3.- Utilizar las etequietas html para mostrar resultados en la pantalla.
+
+*/
