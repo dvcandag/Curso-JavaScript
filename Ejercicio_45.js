5262ed3ea54d114a9522406032d957c4d3b5300f
dvcandag <dvcandag@gmail.com>
2024-04-26 20:03:53 -0500

    Añadiendo ejercicio 45: Estructuras de Control - Condicionales

--- Ejercicio_45.js
+++ Ejercicio_45.js
@@ -1,1 +1,12 @@
+/*
+Ejercicio 45: Estructuras de Control - Condicionales
 
+Crea un programa que solicite al usuario ingresar un número. El programa debe determinar si el número 
+es positivo, negativo o cero, y mostrar un mensaje apropiado en cada caso.
+
+Requisitos:
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+2.- La solución debe entregarse en un archivo .js que se ejecute correctamente.
+3.- Utilizar las etequietas html para mostrar resultados en la pantalla.
+
+*/
