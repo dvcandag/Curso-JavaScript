2660735dac5cc1d713020fd968033534d3c7b355
dvcandag <dvcandag@gmail.com>
2024-04-26 20:35:19 -0500

    Añadiendo ejercicio 47: Estructuras de Control - Condicionales

--- Ejercicio_57.js
+++ Ejercicio_57.js
@@ -1,1 +1,13 @@
+/*
+Ejercicio 57: Estructuras de Control - Condicionales
 
+Desarrolla un juego en el que la computadora "piense" en un número aleatorio entre 1 y 100, y el usuario 
+tenga que adivinarlo. Después de cada intento del usuario, la computadora debe indicar si el número a adivinar 
+es mayor o menor que el número ingresado por el usuario, hasta que el usuario adivine el número correcto.
+
+Requisitos:
+1.- Incluir comentarios explicativos en tu código para brindar claridad sobre su funcionamiento.
+2.- La solución debe entregarse en un archivo .js que se ejecute correctamente.
+3.- Utilizar las etequietas html para mostrar resultados en la pantalla.
+
+*/
