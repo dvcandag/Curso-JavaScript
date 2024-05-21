96e703084a7fa79bdaa226f18c031660add69bee
dvcandag <dvcandag@gmail.com>
2024-05-20 20:12:23 -0500

    Entregando ejercicio 49: Ciclos (Loops) - elementos de un array al revés

--- Ejercicio_49.js
+++ Ejercicio_49.js
@@ -2,4 +2,14 @@
 
 // Imprimir los elementos de un array al revés utilizando un ciclo.
 
+// SOLUCIÓN
 
+const array = [1, 2, 3, 4, 5];
+const longitud = array.length;
+
+console.log("Array original:", array);
+
+console.log("Array al revés:");
+for (let i = longitud - 1; i >= 0; i--) {
+  console.log(array[i]);
+}
