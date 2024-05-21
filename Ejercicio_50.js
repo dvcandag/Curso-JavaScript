7a59f49844ac0c0fcdb457bf7ea021ded90c7c0a
dvcandag <dvcandag@gmail.com>
2024-05-20 20:18:29 -0500

    Ejercicio 50: Manejo de Errores

--- Ejercicio_50.js
+++ Ejercicio_50.js
@@ -1,1 +1,12 @@
+/*
 
+Ejercicio 50: Manejo de Errores
+
+Escribe una función buscarUsuario(nombre) que simule una búsqueda de usuario en una base de datos. Si el nombre de usuario es "admin", lanzará un error
+personalizado indicando que no se puede buscar al usuario administrador. Además, si el nombre es null o undefined, lanzará un error indicando que el 
+nombre es inválido. En caso contrario, la función retornará un objeto representando al usuario encontrado.
+
+Además, se debe escribir un bloque try...catch para probar la función buscarUsuario. Si ocurre algún error, se imprimirá un mensaje de error en la 
+consola, incluyendo información adicional como la fecha en que ocurrió.
+
+*/
