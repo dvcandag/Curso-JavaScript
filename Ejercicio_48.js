621dda2cf24ba752ec4f0cac4fbf9e5d6475f642
dvcandag <dvcandag@gmail.com>
2024-05-20 20:10:27 -0500

    Cargando ejercicio 48: Ciclos (Loops) - suma de números pares

--- Ejercicio_48.js
+++ Ejercicio_48.js
@@ -2,3 +2,12 @@
 
 // Calcular la suma de los números pares del 1 al 10 utilizando un ciclo.
 
+let sumaPares = 0;
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    sumaPares += i;
+  }
+}
+
+console.log("La suma de los números pares del 1 al 10 es: " + sumaPares);
