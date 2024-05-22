a4215e9de875dbcf7a97e5a1c24157aaf566d8ab
dvcandag <dvcandag@gmail.com>
2024-05-21 20:01:01 -0500

    Cargando el avance - Definición de la función dividir

--- Ejercicio_51.js
+++ Ejercicio_51.js
@@ -12,7 +12,25 @@ A continuación, escribe un bloque try...catch para probar la función dividir c
 1.- Llama a la función dividir con dos números donde el divisor no sea cero y muestra el resultado en la consola.
 2.- Llama a la función dividir con un divisor igual a cero y captura el error generado. Muestra un mensaje indicando 
     que ocurrió un error y la descripción del mismo en la consola.
-3.- Recuerda que el manejo de errores es una práctica fundamental en programación para garantizar la robustez y la 
-    fiabilidad de tus aplicaciones.
+
+Recuerda que el manejo de errores es una práctica fundamental en programación para garantizar la robustez y la 
+fiabilidad de tus aplicaciones.
 
 */
+
+// SOLUCIÓN
+
+// Definición de la función dividir
+function dividir(dividendo, divisor) {
+    // Verificar si el divisor es cero
+    if (divisor === 0) {
+        // Si es cero, visualizará un error indicando que no se puede dividir por cero
+        throw new Error('No se puede dividir por cero');
+    }
+
+
+
+
+
+    
+}
