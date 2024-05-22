66cd17ea7aeda760452de772e650959327c39e7e
dvcandag <dvcandag@gmail.com>
2024-05-21 19:46:10 -0500

    Cargando el ejercicio 51: Manejo de Errores

--- Ejercicio_51.js
+++ Ejercicio_51.js
@@ -1,1 +1,18 @@
+/*
+Ejercicio 51: Manejo de Errores
 
+Escribe una función llamada dividir que reciba dos números como argumentos y devuelva el resultado de dividir 
+el primer número por el segundo. Asegúrate de manejar el caso en que el divisor sea cero.
+
+1.- Si el divisor es cero, la función debe lanzar un error indicando que no se puede dividir por cero.
+2.- Si no hay errores, la función debe devolver el resultado de la división.
+
+A continuación, escribe un bloque try...catch para probar la función dividir con diferentes conjuntos de datos de entrada:
+
+1.- Llama a la función dividir con dos números donde el divisor no sea cero y muestra el resultado en la consola.
+2.- Llama a la función dividir con un divisor igual a cero y captura el error generado. Muestra un mensaje indicando 
+    que ocurrió un error y la descripción del mismo en la consola.
+3.- Recuerda que el manejo de errores es una práctica fundamental en programación para garantizar la robustez y la 
+    fiabilidad de tus aplicaciones.
+
+*/
