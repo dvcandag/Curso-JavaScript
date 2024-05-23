83e3dd6897325c1c7ceded0f25bedca128d9568a
dvcandag <dvcandag@gmail.com>
2024-05-22 20:28:03 -0500

    Añadiendo enunciado del Ejercicio 53: break & continue

---
+++ Ejercicio_53.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 53: break & continue
+
+Dado un array de números, encontrar el primer número que sea divisible entre 5 y mayor que 100. Si no se encuentra
+ ningún número que cumpla esta condición, imprimir "No se encontraron números que cumplan los criterios".
+ 
+ */
+
+
