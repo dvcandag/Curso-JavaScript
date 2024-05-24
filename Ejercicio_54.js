bcbffe581c537e00d8fe8577752d69819bfe9068
dvcandag <dvcandag@gmail.com>
2024-05-23 20:35:54 -0500

    Entregando la codificación del Ejercicio 54: Destructuración

--- Ejercicio_54.js
+++ Ejercicio_54.js
@@ -13,3 +13,30 @@ const persona = {
 };
 
 */
+
+// SOLUCIÓN
+
+// Definimos la función obtenerDatos que toma un objeto persona como argumento
+function obtenerDatos(persona) {
+    // Utilizamos la destructuración para extraer el nombre, la edad y la ciudad del objeto persona
+    const { nombre, edad, direccion: { ciudad } } = persona;
+  
+    // Imprimimos los datos obtenidos en la consola
+    console.log(`Nombre: ${nombre}`);
+    console.log(`Edad: ${edad}`);
+    console.log(`Ciudad: ${ciudad}`);
+  }
+  
+  // Objeto persona
+  const persona = {
+    nombre: 'Bladimir',
+    edad: 25,
+    direccion: {
+      ciudad: 'Ciudad de Lima',
+      codigoPostal: '12345'
+    }
+  };
+  
+  // Llamamos a la función obtenerDatos con el objeto persona como argumento
+  obtenerDatos(persona);
+  
