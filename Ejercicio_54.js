1bffb3ac8bcbfe57ac65502c8f83d0e10f4a3835
dvcandag <dvcandag@gmail.com>
2024-05-23 20:34:35 -0500

    Añadiendo el enunciado del Ejercicio 54: Destructuración

---
+++ Ejercicio_54.js
@@ -0,0 +1,15 @@
+/*
+Ejercicio 54: Destructuración
+
+Escribe una función llamada obtenerDatos que tome un objeto como argumento. El objeto tiene la siguiente estructura:
+
+const persona = {
+  nombre: 'Bladimir',
+  edad: 25,
+  direccion: {
+    ciudad: 'Ciudad de Lima',
+    codigoPostal: '12345'
+  }
+};
+
+*/
