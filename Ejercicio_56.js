1c08c2d1f61c4ab9e3297e8a875b5447a313d882
dvcandag <dvcandag@gmail.com>
2024-05-24 20:32:49 -0500

    Enunciado del Ejercicio 56: Objetos Literales

---
+++ Ejercicio_56.js
@@ -0,0 +1,17 @@
+/*
+Ejercicio 56: Objetos Literales
+
+Crear un objeto literal llamado 'producto' que represente un artículo en una tienda en línea. Este objeto debe 
+tener las siguientes propiedades:
+
+1.- 'nombre': que represente el nombre del producto.
+2.- 'precio': que represente el precio del producto en soles.
+3.- 'disponible': que represente si el producto está disponible en stock (true/false).
+4.- 'detalles': que represente una descripción detallada del producto.
+
+Además, crea un método dentro del objeto llamado 'mostrarDetalles' que imprima en la consola los detalles del producto.
+
+A continuación, crea un objeto 'producto2' con diferentes valores y llama al método 'mostrarDetalles' para mostrar los 
+detalles de este producto también."
+
+*/
