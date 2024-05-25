a6cc770983ee536d4cc50f1885d3ee413ee908a4
dvcandag <dvcandag@gmail.com>
2024-05-24 20:19:50 -0500

    Cargando el Ejercicio 55: Destructuración

---
+++ Ejercicio_55.js
@@ -0,0 +1,8 @@
+/*
+Ejercicio 55: Destructuración
+
+Escribe un programa en JavaScript que solicite al usuario ingresar las dimensiones de un 
+rectángulo (largo y ancho) y luego calcule y muestre el perímetro y el área del rectángulo 
+utilizando la destructuración para manejar los datos ingresados por el usuario
+
+*/
