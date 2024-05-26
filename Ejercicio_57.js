92bfad029bc1d1da6f1d59fac672766d3174a708
dvcandag <dvcandag@gmail.com>
2024-05-25 20:06:47 -0500

    Entregando la codificación del Ejercicio 57: Objetos Literales

--- Ejercicio_57.js
+++ Ejercicio_57.js
@@ -23,3 +23,43 @@ Instrucciones:
     de los libros definidos anteriormente (libro1 y libro2).
 
 */
+
+// SOLUCIÓN
+
+// Definición de objetos literales para representar información sobre libros
+
+// Libro 1: Objeto literal que representa información sobre el primer libro
+const libro1 = {
+    titulo: "Cien años de soledad",
+    autor: "Gabriel García Márquez",
+    genero: "Realismo mágico",
+    añoPublicacion: 1967,
+    paginas: 432
+  };
+  
+  // Libro 2: Objeto literal que representa información sobre el segundo libro
+  const libro2 = {
+    titulo: "El nombre del viento",
+    autor: "Patrick Rothfuss",
+    genero: "Fantasía",
+    añoPublicacion: 2007,
+    paginas: 662
+  };
+  
+  // Función para mostrar información detallada de un libro
+  function mostrarInformacionLibro(libro) {
+    console.log(`Título: ${libro.titulo}`);
+    console.log(`Autor: ${libro.autor}`);
+    console.log(`Género: ${libro.genero}`);
+    console.log(`Año de Publicación: ${libro.añoPublicacion}`);
+    console.log(`Páginas: ${libro.paginas}`);
+    console.log("-----------------------");
+  }
+  
+  // Muestra información detallada de los libros utilizando la función mostrarInformacionLibro
+  console.log("Información del Libro 1:");
+  mostrarInformacionLibro(libro1);
+  
+  console.log("Información del Libro 2:");
+  mostrarInformacionLibro(libro2);
+  
