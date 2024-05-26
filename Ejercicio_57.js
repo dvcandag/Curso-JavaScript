00158b66796c80cd64ca20d7de976f368618841b
dvcandag <dvcandag@gmail.com>
2024-05-25 20:04:20 -0500

    Cargando el enunciado del Ejercicio 57: Objetos Literales

---
+++ Ejercicio_57.js
@@ -0,0 +1,25 @@
+/*
+Ejercicio 57: Objetos Literales
+
+Aplicando el uso de los objetos literales en JavaScript, representar información 
+sobre libros y cómo acceder a esa información utilizando funciones.
+
+Instrucciones:
+
+1.- Definir dos objetos literales, libro1 y libro2, que representen información sobre 
+    dos libros diferentes. Cada objeto debe contener las siguientes propiedades:
+
+    a.- titulo: El título del libro.
+    b.- autor: El autor del libro.
+    c.- genero: El género literario del libro.
+    d.- añoPublicacion: El año en que fue publicado el libro.
+    f.- paginas: El número de páginas del libro.
+
+2.- Crear una función llamada (mostrarInformacionLibro) que tome un objeto libro como argumento y 
+    muestre la información detallada del libro en la consola. La información detallada debe incluir 
+    el título, autor, género, año de publicación y número de páginas del libro.
+
+3.- Utiliza la función (mostrarInformacionLibro) para mostrar la información detallada de cada uno 
+    de los libros definidos anteriormente (libro1 y libro2).
+
+*/
