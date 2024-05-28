6b00c83661c146c9bd83dd087f20561a1af108fd
dvcandag <dvcandag@gmail.com>
2024-05-27 20:06:30 -0500

    Añadienddo enunciado del Ejercicio 59: Parámetros REST & Operador Spread

---
+++ Ejercicio_59.js
@@ -0,0 +1,7 @@
+/*
+Ejercicio 59: Parámetros REST & Operador Spread
+
+Implementa una función que, mediante el uso de parámetros REST, pueda recibir un número variable de strings que representan 
+nombres de estudiantes. Esta función deberá procesar estos nombres y devolver un mensaje que los presente ordenados alfabéticamente, 
+junto con la cantidad total de estudiantes recibidos.
+*/
