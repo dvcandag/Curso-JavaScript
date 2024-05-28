e163d500018ae7896eaae705a2cc1c39bdb4854d
dvcandag <dvcandag@gmail.com>
2024-05-27 20:09:21 -0500

    Cargando enunciado del Ejercicio 60: Parámetros REST & Operador Spread

---
+++ Ejercicio_60.js
@@ -0,0 +1,7 @@
+/*
+Ejercicio 60: Parámetros REST & Operador Spread
+
+Desarrolla una función que, utilizando parámetros REST, acepte un número variable de arrays numéricos como argumentos. Luego, utilizando 
+el operador spread, combine todos los elementos de estos arrays en uno solo. Finalmente, la función deberá devolver un nuevo array que 
+contenga todos los números ordenados de manera ascendente.
+*/
