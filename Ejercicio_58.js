96e5246b85694c71ceb5ac7b89065a27b9dd81e3
dvcandag <dvcandag@gmail.com>
2024-05-27 20:04:32 -0500

    Añadiendo el enunciado del Ejercicio 58: Parámetros REST & Operador Spread

---
+++ Ejercicio_58.js
@@ -0,0 +1,8 @@
+/*
+Ejercicio 58: Parámetros REST & Operador Spread
+
+Implementar una función que acepte un número variable de argumentos numéricos y retorne un array que contenga dichos números 
+ordenados de manera ascendente, utilizando un algoritmo de ordenamiento eficiente. El resultado debe reflejar una secuencia 
+donde los valores sean crecientes de menor a mayor
+
+*/
