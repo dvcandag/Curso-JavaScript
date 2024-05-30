2ff514cf50af6429cf0e8c0c8c3ebc8e00774a50
dvcandag <dvcandag@gmail.com>
2024-05-29 20:46:37 -0500

    Cargando el enunciado del Ejercicio 61: Arrow Functions

---
+++ Ejercicio_61.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 61: Arrow Functions
+
+Desarrollar una función (filtrarNumerosPares) que tome un array de números como entrada y devuelva un nuevo array que contenga 
+solo los números pares del array original. 
+
+Utilizar Arrow Functions en tu solución.
+
+*/
