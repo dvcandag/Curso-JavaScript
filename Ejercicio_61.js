d93bbf1a8ce9594008e4a201a9e731fb68b90dac
dvcandag <dvcandag@gmail.com>
2024-05-30 20:01:31 -0500

    Entregado el desarrollo del Ejercicio 61: Arrow Functions

--- Ejercicio_61.js
+++ Ejercicio_61.js
@@ -7,3 +7,13 @@ solo los números pares del array original.
 Utilizar Arrow Functions en tu solución.
 
 */
+
+// SOLUCIÓN
+
+// Definición de la función filtrarNumerosPares utilizando Arrow Function
+const filtrarNumerosPares = (numeros) => numeros.filter(numero => numero % 2 === 0);
+
+// Ejemplo de uso de la función con un array de prueba
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numerosPares = filtrarNumerosPares(numeros);
+console.log(numerosPares); // resultado: [2, 4, 6, 8, 10]
