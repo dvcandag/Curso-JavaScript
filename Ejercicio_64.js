3a82d9faea55e484816ff59edc8e9cf71ebde466
dvcandag <dvcandag@gmail.com>
2024-05-30 20:49:09 -0500

    Añadiendo enunciado del Ejercicio 64: Objeto console

---
+++ Ejercicio_64.js
@@ -0,0 +1,7 @@
+/*
+Ejercicio 64: Objeto console
+
+Crea un objeto que represente a una persona con propiedades como nombre, edad y ocupación. Luego, utiliza el 
+método dir() del objeto console para mostrar información detallada sobre este objeto en la consola.
+
+*/
