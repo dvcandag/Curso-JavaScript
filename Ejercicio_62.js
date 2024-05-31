f1bc8abc63c3123133b3c800ad7ea374d813788f
dvcandag <dvcandag@gmail.com>
2024-05-30 20:32:53 -0500

    Cargando enunciado del Ejercicio 62: Arrow Functions (Funciones de flecha)

---
+++ Ejercicio_62.js
@@ -0,0 +1,14 @@
+/*
+Ejercicio 62: Arrow Functions (Funciones de flecha)
+
+Desarrolla una función llamada areaCirculo utilizando arrow functions. 
+Esta función debe tomar el radio de un círculo como parámetro y devolver 
+el área correspondiente del círculo
+
+La función debe realizar las siguientes tareas:
+
+1.- Validar que el radio proporcionado sea un número positivo. Si el radio 
+    no es válido, la función debe devolver un mensaje de error: "Error: El radio debe ser un número positivo."
+2.- Calcular el área del círculo utilizando la fórmula mencionada.
+3.- Devolver el área calculada.
+*/
