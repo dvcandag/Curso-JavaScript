aeae114834082fe9688ea32cfe75756d85e28381
dvcandag <dvcandag@gmail.com>
2024-06-01 21:23:08 -0500

    Añadiendo el enunciado del Ejercicio 65: Objeto console

---
+++ Ejercicio_65.js
@@ -0,0 +1,7 @@
+/*
+Ejercicio 65: Objeto console
+
+Escribe una función que realice algún cálculo o manipulación de datos y utilice el método group() y 
+groupEnd() del objeto console para agrupar y mostrar los pasos intermedios del proceso en la consola.
+
+*/
