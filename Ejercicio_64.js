3f07b72da03830ed180ea3df45b9ca774fa024c3
dvcandag <dvcandag@gmail.com>
2024-06-03 20:33:28 -0500

    Cargando Ejercicio 64: Objeto console

--- Ejercicio_64.js
+++ Ejercicio_64.js
@@ -5,3 +5,16 @@ Crea un objeto que represente a una persona con propiedades como nombre, edad y 
 método dir() del objeto console para mostrar información detallada sobre este objeto en la consola.
 
 */
+
+// SOLUCION
+
+// Crear un objeto que represente a una persona
+const persona = {
+    nombre: "Juan",
+    edad: 30,
+    ocupacion: "Ingeniero"
+  };
+  
+  // Utilizar el método dir() para mostrar información detallada sobre el objeto persona
+  console.dir(persona);
+  
