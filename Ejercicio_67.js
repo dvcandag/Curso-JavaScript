de789372c62f990abab9c434878cd64ae9672478
dvcandag <dvcandag@gmail.com>
2024-06-11 20:51:16 -0500

    Cargando Ejercicio 67: Objeto Math - Área de un círculo

--- Ejercicio_67.js
+++ Ejercicio_67.js
@@ -6,3 +6,9 @@ Crea una función en JavaScript que calcule el área de un círculo utilizando e
 Utiliza el objeto Math para realizar los cálculos necesarios. La fórmula para calcular el área de un círculo 
 */
 
+// SOLUCIÓN
+// Llama a la función y proporciona el radio del círculo
+
+let radio = 5;
+let area = calcularAreaCirculo(radio);
+console.log("El área del círculo con radio", radio, "es:", area);
