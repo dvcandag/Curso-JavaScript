224d705768927b1f1c3a14f03cfcc17d74cb54b3
dvcandag <dvcandag@gmail.com>
2024-06-11 21:01:09 -0500

    Enunciado del Ejercicio 68: Objeto Math - Número aleatorio - uso del formula Box-Muller

--- Ejercicio_68.js
+++ Ejercicio_68.js
@@ -1,1 +1,17 @@
+/*
+Ejercicio 68: Objeto Math - Número aleatorio - uso del formula Box-Muller
+
+Desarrolla una función que genere un número aleatorio dentro de un rango especificado por el usuario, pero 
+con una distribución normal (gaussiana). Utiliza el objeto Math para realizar los cálculos necesarios.
+
+Instrucciones:
+
+1.- Escribe una función llamada generarNumeroAleatorioGaussiano que tome dos parámetros: media y desviacionEstandar.
+2.- Dentro de la función, utiliza el método random() del objeto Math para generar dos números aleatorios uniformemente 
+    distribuidos entre 0 y 1. Estos números representarán puntos en un plano cartesiano.
+3.- Utiliza la transformación de Box-Muller para convertir estos números aleatorios en números aleatorios con una distribución normal.
+4.- Devuelve el número aleatorio 𝑋
+5.- Prueba tu función con diferentes valores de media y desviación estándar para asegurarte de que funcione correctamente.
+
+*/
 
