be0514a9d6dc42133175e695d4ca007eb4ee97e2
dvcandag <dvcandag@gmail.com>
2024-06-14 20:04:09 -0500

    Añadiendo Ejercicio 69: Objetos y Funciones del Lenguaje - Operador de Cortocircuito

--- Ejercicio_69.js
+++ Ejercicio_69.js
@@ -1,1 +1,8 @@
-Ejercicio
+/*
+Ejercicio 69: Objetos y Funciones del Lenguaje - Operador de Cortocircuito
+
+Implementa una función que utilice el operador de cortocircuito para determinar si una cadena de texto 
+es válida. La función debe devolver true si la cadena no es nula ni está indefinida, y además tiene al menos una 
+longitud de 1 carácter. De lo contrario, debe devolver false."
+
+*/
