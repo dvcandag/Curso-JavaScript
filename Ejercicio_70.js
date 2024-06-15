952afd7fee55587cb282b8bb7318145906b07dc9
dvcandag <dvcandag@gmail.com>
2024-06-14 20:06:14 -0500

    Añadiendo Ejercicio 70: Objetos y Funciones del Lenguaje - Operador de Cortocircuito

---
+++ Ejercicio_70.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 70: Objetos y Funciones del Lenguaje - Operador de Cortocircuito
+
+Desarrolla una función que utilice el operador de cortocircuito para calcular el promedio 
+de un arreglo de números enteros positivos. La función debe asegurarse de que el arreglo no esté vacío y 
+que todos sus elementos sean números enteros positivos. En caso de cumplirse estas condiciones, debe 
+devolver el promedio de los números. Si alguna condición no se cumple, la función deberá devolver NaN.
+
+*/
