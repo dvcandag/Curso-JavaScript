0ac4f0efb847db5d5286e927726dcf746b3ae46d
dvcandag <dvcandag@gmail.com>
2024-06-24 20:42:24 -0500

    Añadiendo enunciado del Ejercicio 72: Objetos y Funciones del Lenguaje - alert y confirm

---
+++ Ejercicio_72.js
@@ -0,0 +1,7 @@
+/*
+Ejercicio 72: Objetos y Funciones del Lenguaje - alert y confirm
+
+Desarrolla un programa que solicite confirmación al usuario para borrar un archivo utilizando "confirm". Si el usuario confirma, 
+muestra un mensaje de "Archivo borrado" con un "alert". Si el usuario cancela, muestra un mensaje de "Operación cancelada" con un "alert".
+
+*/
