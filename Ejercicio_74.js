9795f9f65103173cc0b9bdaf020d114b75f9eb55
dvcandag <dvcandag@gmail.com>
2024-06-24 20:50:10 -0500

    Ejercicio 74: Objetos y Funciones del Lenguaje - prompt y alert

---
+++ Ejercicio_74.js
@@ -0,0 +1,10 @@
+/*
+Ejercicio 74: Objetos y Funciones del Lenguaje - prompt y alert
+
+Verificación de Edad
+
+
+Desarrolla un programa que solicite al usuario su edad mediante un "prompt" y luego verifique si es 
+mayor de edad (18 años o más). Muestra un mensaje utilizando un "alert" indicando si es mayor de edad o no.
+
+*/
