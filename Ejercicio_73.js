7c5397bd20c5a28b98e6f584645f7329fa4c8a89
dvcandag <dvcandag@gmail.com>
2024-06-24 20:46:03 -0500

    Añadiendo enunciado del Ejercicio 73: Objetos y Funciones del Lenguaje - prompt y alert

---
+++ Ejercicio_73.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 73: Objetos y Funciones del Lenguaje - prompt y alert
+
+Calculadora de Promedio
+
+Desarrolla un programa que solicite al usuario una lista de números separados por comas utilizando "prompt". 
+Luego calcula el promedio de esos números y muestra el resultado utilizando un "alert".
+
+*/
