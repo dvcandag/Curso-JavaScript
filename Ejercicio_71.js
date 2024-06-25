d1fa52cfdbd7aa4b9e497704efd778ea4247197d
dvcandag <dvcandag@gmail.com>
2024-06-24 20:38:09 -0500

    Añadiendo enunciado del Ejercicio 71: Objetos y Funciones del Lenguaje - alert, confirm y prompt

---
+++ Ejercicio_71.js
@@ -0,0 +1,7 @@
+/*
+Ejercicio 71: Objetos y Funciones del Lenguaje - alert, confirm y prompt
+
+Desarrolla un programa que solicite al usuario su nombre mediante un prompt y luego muestre 
+un saludo personalizado utilizando un alert.
+
+*/
