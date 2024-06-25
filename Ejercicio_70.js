5c6de36eeca8adbc630970010f74ca1cc0a12376
dvcandag <dvcandag@gmail.com>
2024-06-24 20:23:54 -0500

    Entregando Ejercicio 70: Objetos y Funciones del Lenguaje - Operador de Cortocircuito

--- Ejercicio_70.js
+++ Ejercicio_70.js
@@ -7,3 +7,27 @@ que todos sus elementos sean números enteros positivos. En caso de cumplirse es
 devolver el promedio de los números. Si alguna condición no se cumple, la función deberá devolver NaN.
 
 */
+
+// SOLUCIÓN
+
+function calcularPromedio(arr) {
+    // Verifica que el arreglo no esté vacío y que todos los elementos sean números enteros positivos
+    if (arr.length === 0 || !arr.every(num => Number.isInteger(num) && num > 0)) {
+        return NaN;
+    }
+
+    // Calcula la suma de los elementos del arreglo
+    let suma = arr.reduce((acc, num) => acc + num, 0);
+    
+    // Calcula el promedio
+    let promedio = suma / arr.length;
+
+    return promedio;
+}
+
+// Resultados esperados
+console.log(calcularPromedio([1, 2, 3, 4, 5]));  // Salida: 3
+console.log(calcularPromedio([]));  // Salida: NaN
+console.log(calcularPromedio([1, 2, -3, 4, 5]));  // Salida: NaN
+console.log(calcularPromedio([1, 2, 3.5, 4, 5]));  // Salida: NaN
+console.log(calcularPromedio([10, 20, 30, 40, 50]));  // Salida: 30
