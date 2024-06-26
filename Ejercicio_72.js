2bef6f959473a37d5d351ff9b14873424b3fcd8b
dvcandag <dvcandag@gmail.com>
2024-06-25 19:55:20 -0500

    Entregando la solución del Ejercicio 72: Objetos y Funciones del Lenguaje - alert y confirm

--- Ejercicio_72.js
+++ Ejercicio_72.js
@@ -5,3 +5,13 @@ Desarrolla un programa que solicite confirmación al usuario para borrar un arch
 muestra un mensaje de "Archivo borrado" con un "alert". Si el usuario cancela, muestra un mensaje de "Operación cancelada" con un "alert".
 
 */
+
+// SOLUCIÓN
+
+// Ejercicio 2: Confirmación de Borrado
+let confirmacion = confirm("¿Estás seguro de que deseas borrar este archivo?");
+if (confirmacion) {
+    alert("Archivo borrado.");
+} else {
+    alert("Operación cancelada.");
+}
