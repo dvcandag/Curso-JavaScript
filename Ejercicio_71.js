6cc0e2627fab17bf3f85da175d1fda6d4e3a699b
dvcandag <dvcandag@gmail.com>
2024-06-25 19:52:52 -0500

    Entregando Ejercicio 71: Objetos y Funciones del Lenguaje - alert, confirm y prompt

--- Ejercicio_71.js
+++ Ejercicio_71.js
@@ -5,3 +5,15 @@ Desarrolla un programa que solicite al usuario su nombre mediante un prompt y lu
 un saludo personalizado utilizando un alert.
 
 */
+
+// SOLUCIÓN
+
+// Ejercicio 71: Saludo personalizado
+let nombre = prompt("Por favor, ingresa tu nombre:");
+if (nombre) {
+    alert("Hola, " + nombre + "! Bienvenido/a.");
+} else {
+    alert("No ingresaste tu nombre.");
+}
+
+//Ejecutar desde el navegador
