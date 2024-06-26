e8199f6cbdb90a1a06c34168afbc4bcc0725d973
dvcandag <dvcandag@gmail.com>
2024-06-25 19:46:58 -0500

    Enunciado del Ejercicio 75: Objetos y Funciones del Lenguaje - prompt y alert

---
+++ Ejercicio_75.js
@@ -0,0 +1,8 @@
+/*
+Ejercicio 75: Objetos y Funciones del Lenguaje - prompt y alert
+
+Ejercicio 75: Adivinanza
+Desarrolla un programa que seleccione un número aleatorio entre 1 y 10. Luego solicita al usuario 
+adivinar el número utilizando "prompt". Si el usuario adivina correctamente, muestra un mensaje de 
+felicitación con un "alert". Si no, indica cuál era el número correcto utilizando un "alert".
+*/
