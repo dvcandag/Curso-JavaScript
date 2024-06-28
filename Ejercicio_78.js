595c1d5162c7db2cfe970e61de0e925ded4a5416
dvcandag <dvcandag@gmail.com>
2024-06-27 20:40:07 -0500

    Cargando Ejercicio 78: Objetos y Funciones del Lenguaje - Expresiones Regulares

---
+++ Ejercicio_78.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 78: Objetos y Funciones del Lenguaje - Expresiones Regulares
+
+Ejercicio 78: Verificación de correo electrónico
+Desarrolla un programa que solicite al usuario ingresar su dirección de correo electrónico mediante 
+un "prompt". Luego verifica si la dirección de correo electrónico es válida utilizando una expresión 
+regular. Si el correo electrónico es válido, muestra un mensaje de felicitación utilizando un "alert". 
+Si no, muestra un mensaje de error indicando que el correo electrónico no es válido utilizando un "alert".
+*/
