3d0db41974d6bcd43470f169eec5a3266cbc1d8e
dvcandag <dvcandag@gmail.com>
2024-06-27 20:44:04 -0500

    Cargando Ejercicio 79: Objetos y Funciones del Lenguaje - Expresiones Regulares

---
+++ Ejercicio_79.js
@@ -0,0 +1,11 @@
+/*
+Ejercicio 79: Objetos y Funciones del Lenguaje - Expresiones Regulares
+
+Ejercicio 79: Verificación de número de teléfono
+Desarrolla un programa que solicite al usuario ingresar su número de teléfono mediante 
+un "prompt". Luego verifica si el número de teléfono es válido utilizando una expresión 
+regular. Un número de teléfono válido debe tener exactamente 9 dígitos y comenzar con el 
+número 9. Si el número de teléfono es válido, muestra un mensaje de felicitación utilizando 
+un "alert". Si no, muestra un mensaje de error indicando que el número de teléfono 
+no es válido utilizando un "alert".
+*/
